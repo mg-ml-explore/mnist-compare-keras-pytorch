- corrected total loss calculation

pytorch_mnist/train_test_utils.py
@@ -7,7 +7,7 @@ from torchvision.datasets import MNIST
 from . import data_utils
 
 
-def train_single_epoch(model: nn.Module, train_loader: DataLoader, optimizer, epoch, device: str):
+def train_single_epoch(model: nn.Module, train_loader: DataLoader, optimizer, device: str):
     model.train()
 
     train_loss = 0
@@ -20,11 +20,12 @@ def train_single_epoch(model: nn.Module, train_loader: DataLoader, optimizer, ep
         loss.backward()
         optimizer.step()
 
-        train_loss += loss.item()
+        train_loss += loss.item() * len(data)
         pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
         train_correct += pred.eq(target.view_as(pred)).sum().item()
 
-    print('\Train set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    train_loss = train_loss / len(train_loader.dataset)
+    print('\nTrain set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         train_loss, train_correct, len(train_loader.dataset),
         100. * train_correct / len(train_loader.dataset)))
 
@@ -36,11 +37,12 @@ def test(model: nn.Module, test_loader: DataLoader, device: str):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.cross_entropy(output, target).item()  # sum up batch loss
+            test_loss += F.cross_entropy(output, target).item() * len(data)  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    test_loss = test_loss / len(test_loader.dataset)
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
@@ -53,7 +55,8 @@ def train(model: nn.Module, train_valid_data: MNIST, device, use_cuda, batch_siz
     optimizer = optim.Adam(model.parameters())
 
     for epoch in range(1, epochs):
-        train_single_epoch(model, train_loader, optimizer, epoch, device)
+        print(f"--- Epoch {epoch} ---")
+        train_single_epoch(model, train_loader, optimizer, device)
         test(model, valid_loader, device)
 
     # torch.save(model.state_dict(), "mnist_cnn.pt")